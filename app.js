7b993c8c3f1d26568debc506d2fbaf0079891dbf
Thomaslam2142 <thomaslam2142@gmail.com>
2023-09-11 18:38:48 -0400

    base functionality working

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 const calculateBtn = document.querySelector(".calculateButton");
+let costTotal = document.querySelector(".costElement");
+
 
 calculateBtn.addEventListener("click", ()=>{
-    console.log("button clicked");
+    let inputAmount = document.querySelector(".preAmount").value;
+    let tipPercentage = document.querySelector(".tipPercentage").value;
+    let total = 0;
+
+    tipPercentage = (tipPercentage / 100)+1;
+    console.log(tipPercentage);
+    total = (inputAmount * tipPercentage);
+    console.log(total);
+    
+
+    costTotal.textContent = Math.round(total,2);
+
+    
 })
+
